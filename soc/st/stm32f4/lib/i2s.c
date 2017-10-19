61cf405ca5e7416fd10ebeed9d09836d23a1fa1f
Jason Harris <sirmanlypowers@gmail.com>
2017-10-19 13:23:58 -0700

    WIP. I2S driver.

--- soc/st/stm32f4/lib/i2s.c
+++ soc/st/stm32f4/lib/i2s.c
@@ -7,6 +7,7 @@ I2S Driver
 //-----------------------------------------------------------------------------
 
 #include <string.h>
+#include <assert.h>
 
 #include "stm32f4_soc.h"
 
@@ -41,6 +42,7 @@ static void spi_enable(struct i2s_drv *i2s) {
 //-----------------------------------------------------------------------------
 
 int i2s_init(struct i2s_drv *i2s, struct i2s_cfg *cfg) {
+	uint32_t val;
 
 	memset(i2s, 0, sizeof(struct i2s_drv));
 	i2s->cfg = *cfg;
@@ -48,9 +50,33 @@ int i2s_init(struct i2s_drv *i2s, struct i2s_cfg *cfg) {
 	spi_enable(i2s);
 
 	i2s->base = spi_base(i2s->cfg.idx);
-	i2s->base->I2SCFGR &= ~(SPI_I2SCFGR_CHLEN | SPI_I2SCFGR_DATLEN | SPI_I2SCFGR_CKPOL | SPI_I2SCFGR_I2SSTD | SPI_I2SCFGR_PCMSYNC | SPI_I2SCFGR_I2SCFG | SPI_I2SCFGR_I2SE | SPI_I2SCFGR_I2SMOD);
-	i2s->base->I2SPR = 2U;
 
+	// clear I2SCFGR - preserve reserved bits
+	i2s->base->I2SCFGR &= (15 << 12) | (1 << 6);
+	// set I2SPR to the reset value
+	i2s->base->I2SPR = 2;
+
+	// setup I2SCFGR
+	val = i2s->base->I2SCFGR;
+	val |= (1 << 11 /*I2SMOD */ );	// I2S is enabled
+	val |= (0 << 10 /*I2SE */ );	// I2S peripheral is disabled
+	val |= (i2s->cfg.mode | i2s->cfg.standard | i2s->cfg.clk_polarity | i2s->cfg.data_format);
+	i2s->base->I2SCFGR = val;
+
+	// TODO support full duplex mode
+	assert(i2s->cfg.fdx_mode == 0);
+
+#if 0
+
+	haudio_in_i2s.Init.AudioFreq = 4 * AudioFreq;
+	haudio_in_i2s.Init.ClockSource = I2S_CLOCK_PLL;
+	haudio_in_i2s.Init.CPOL = I2S_CPOL_HIGH;
+	haudio_in_i2s.Init.DataFormat = I2S_DATAFORMAT_16B;
+	haudio_in_i2s.Init.MCLKOutput = I2S_MCLKOUTPUT_DISABLE;
+	haudio_in_i2s.Init.Mode = I2S_MODE_MASTER_RX;
+	haudio_in_i2s.Init.Standard = I2S_STANDARD_LSB;
+
+#endif
 	return 0;
 }
 
--- soc/st/stm32f4/lib/i2s.h
+++ soc/st/stm32f4/lib/i2s.h
@@ -17,14 +17,39 @@ I2S Driver
 
 //-----------------------------------------------------------------------------
 
+// operating modes (I2SCFG)
+#define I2S_MODE_SLAVE_TX (0 << 8)
+#define I2S_MODE_SLAVE_RX (1 << 8)
+#define I2S_MODE_MASTER_TX (2 << 8)
+#define I2S_MODE_MASTER_RX (3 << 8)
+
+// i2s standard (I2SSTD, PCMSYNC)
+#define I2S_STANDARD_PHILIPS (0 << 4)
+#define I2S_STANDARD_MSB (1 << 4)
+#define I2S_STANDARD_LSB (2 << 4)
+#define I2S_STANDARD_PCM_SHORT ((3 << 4) | (0 << 7))
+#define I2S_STANDARD_PCM_LONG ((3 << 4) | (1 << 7))
+
+// data format (DATLEN, CHLEN)
+#define I2S_DATAFORMAT_16B ((0 << 1 /*datlen=16*/) | (0 << 0 /*chlen = 16*/))
+#define I2S_DATAFORMAT_16B_EXTENDED ((0 << 1 /*datlen=16*/) | (1 << 0 /*chlen=32*/))
+#define I2S_DATAFORMAT_24B ((1 << 1 /*datlen=24*/) | (1 << 1 /*chlen=32*/))
+#define I2S_DATAFORMAT_32B ((2 << 1 /*datlen=32*/) | (1 << 1 /*chlen=32*/))
+
+// clock polarity (CKPOL)
+#define I2S_CPOL_LOW (0 << 3)
+#define I2S_CPOL_HIGH (1 << 3)
+
+//-----------------------------------------------------------------------------
+
 struct i2s_cfg {
 	int idx;		// i2s device to use
-	int mode;		// operating mode
-	int standard;		// standard used
-	int data_format;	// data format
+	uint32_t mode;		// operating mode
+	uint32_t standard;	// standard used
+	uint32_t data_format;	// data format
 	int mclk_output;	// is mclk output enabled?
 	int audio_freq;		// frequency
-	int clk_polarity;	// clock polarity
+	uint32_t clk_polarity;	// clock polarity
 	int clk_src;		// clock source
 	int fdx_mode;		// is full duplex mode enabled?
 };
