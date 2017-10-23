4dae675fed001145fe2c3ad779c6057e911ede14
Jason Harris <sirmanlypowers@gmail.com>
2017-10-22 21:03:05 -0700

    WIP. DMA driver.

--- soc/st/stm32f4/lib/dma.c
+++ soc/st/stm32f4/lib/dma.c
@@ -6,6 +6,7 @@ DMA Driver
 */
 //-----------------------------------------------------------------------------
 
+#include <assert.h>
 #include <string.h>
 
 #define DEBUG
@@ -16,27 +17,7 @@ DMA Driver
 
 //-----------------------------------------------------------------------------
 
-#define DMA_SxCR_MASK   0x0fefffffU
-#define DMA_SxNDTR_MASK 0x0000ffffU
-#define DMA_SxFCR_MASK  0x000000bfU
-
-//-----------------------------------------------------------------------------
-
-// return a pointer to the dma controller registers (1,2)
-static inline DMA_TypeDef *dma_controller_regs(int controller) {
-	static const uint32_t regs[] = { 0, DMA1_BASE, DMA2_BASE };
-	return (DMA_TypeDef *) regs[controller];
-}
-
-// return a pointer to the dma stream registers (0..7)
-static inline DMA_Stream_TypeDef *dma_stream_regs(int controller, int stream) {
-	static const uint32_t ofs[] = { 0x10, 0x28, 0x40, 0x58, 0x70, 0x88, 0xA0, 0xB8 };
-	return (DMA_Stream_TypeDef *) ((uint32_t) dma_controller_regs(controller) + ofs[stream]);
-}
-
-//-----------------------------------------------------------------------------
-
-// enable the clock to the dma module
+// enable the clock to the dma controller module
 static void dma_module_enable(int controller) {
 	if (controller == 1) {
 		RCC->AHB1ENR |= (1 << 21 /*DMA1EN */ );
@@ -47,26 +28,82 @@ static void dma_module_enable(int controller) {
 
 //-----------------------------------------------------------------------------
 
-void dma_enable(struct dma_drv *dma) {
-	dma->sregs->CR |= (1 << 0 /*EN*/);
+// return a pointer to the dma base registers (1,2)
+static inline DMA_TypeDef *dma_base_regs(struct dma_drv *dma) {
+	static const uint32_t regs[] = { 0, DMA1_BASE, DMA2_BASE };
+	return (DMA_TypeDef *) regs[dma->cfg.controller];
 }
 
-void dma_disable(struct dma_drv *dma) {
-	dma->sregs->CR &= ~(1 << 0 /*EN*/);
+// return a pointer to the dma stream registers (0..7)
+static inline DMA_Stream_TypeDef *dma_stream_regs(struct dma_drv *dma) {
+	static const uint8_t ofs[] = { 0x10, 0x28, 0x40, 0x58, 0x70, 0x88, 0xA0, 0xB8 };
+	return (DMA_Stream_TypeDef *) ((uint32_t) dma_base_regs(dma) + ofs[dma->cfg.stream]);
+}
+
+// return a pointer to the dma interrupt registers
+static inline DMA_Interrupt_TypeDef *dma_interrupt_regs(struct dma_drv *dma) {
+	uint8_t ofs = dma->cfg.stream & 4;
+	return (DMA_Interrupt_TypeDef *) ((uint32_t) dma_base_regs(dma) + ofs);
 }
 
 //-----------------------------------------------------------------------------
+
+// clear dma interrupt flags
+static inline void dma_clr_interrupt_flags(struct dma_drv *dma) {
+	static const uint8_t shift[] = { 0, 6, 16, 22 };
+	dma->iregs->IFCR = 0x3d << shift[dma->cfg.stream & 3];
+}
+
+//-----------------------------------------------------------------------------
+
+// setup a peripheral/memory transfer
+static void dma_peripheral_transfer(struct dma_drv *dma, uint32_t dst, uint32_t src, uint32_t length) {
+	uint32_t dir;
+	// clear double buffering
+	dma->sregs->CR &= ~(1 << 18 /*DBM*/);
+	// set the length to transfer
+	dma->sregs->NDTR = length;
+	dir = (dma->sregs->CR >> 6 /*DIR*/) & 3;
+	if (dir == 0) {
+		// peripheral to memory
+		dma->sregs->PAR = src;
+		dma->sregs->M0AR = dst;
+	} else if (dir == 1) {
+		// memory to peripheral
+		dma->sregs->PAR = dst;
+		dma->sregs->M0AR = src;
+	}
+}
+
+// start a peripheral/memory transfer
+int dma_start(struct dma_drv *dma, uint32_t dst, uint32_t src, uint32_t length) {
+	dma_peripheral_transfer(dma, dst, src, length);
+	dma_enable(dma);
+	return 0;
+}
+
+//-----------------------------------------------------------------------------
+
+// define the non-reserved register bits
+#define DMA_SxCR_MASK   0x0fefffffU
+#define DMA_SxNDTR_MASK 0x0000ffffU
+#define DMA_SxFCR_MASK  0x000000bfU
 
 int dma_init(struct dma_drv *dma, struct dma_cfg *cfg) {
 	uint32_t val;
 
 	memset(dma, 0, sizeof(struct dma_drv));
+	cfg->stream &= 7;
+	cfg->controller &= 3;
 	dma->cfg = *cfg;
-	dma->cregs = dma_controller_regs(dma->cfg.controller);
-	dma->sregs = dma_stream_regs(dma->cfg.controller, dma->cfg.stream);
+	dma->iregs = dma_interrupt_regs(dma);
+	dma->sregs = dma_stream_regs(dma);
 
 	// enable the dma controller
 	dma_module_enable(dma->cfg.controller);
+
+	// disable the stream to allow modifications
+	dma_disable(dma);
 
 	// SxCR setup
 	val = (0 << 25 /*CHSEL*/);	// Channel selection (0)
@@ -83,24 +120,26 @@ int dma_init(struct dma_drv *dma, struct dma_cfg *cfg) {
 	val |= (1 << 8 /*CIRC*/);	// Circular mode (on)
 	val |= (1 << 6 /*DIR*/);	// Data transfer direction (memory to peripheral)
 	val |= (0 << 5 /*PFCTRL*/);	// Peripheral flow controller (dma)
-	val |= (1 << 4 /*TCIE*/);	// Transfer complete interrupt enable (on)
-	val |= (1 << 3 /*HTIE*/);	// Half transfer interrupt enable (on)
-	val |= (1 << 2 /*TEIE*/);	// Transfer error interrupt enable (on)
-	val |= (1 << 1 /*DMEIE*/);	// Direct mode error interrupt enable (on)
+	val |= (0 << 4 /*TCIE*/);	// Transfer complete interrupt enable (off)
+	val |= (0 << 3 /*HTIE*/);	// Half transfer interrupt enable (off)
+	val |= (0 << 2 /*TEIE*/);	// Transfer error interrupt enable (off)
+	val |= (0 << 1 /*DMEIE*/);	// Direct mode error interrupt enable (off)
 	val |= (0 << 0 /*EN*/);	// Stream enable / flag stream ready when read low (disabled)
-	dma->sregs->CR &= ~(1 << 0 /*EN*/);	// allow modifications
 	reg_rmw(&dma->sregs->CR, DMA_SxCR_MASK, val);
 
 	dma->sregs->NDTR = 0;
-	dma->sregs->PAR = dma->cfg.par;
-	dma->sregs->M0AR = dma->cfg.mar;
+	dma->sregs->PAR = 0;
+	dma->sregs->M0AR = 0;
 	dma->sregs->M1AR = 0;
 
 	// SxFCR setup
-	val = (1 << 7 /*FEIE*/);	// FIFO error interrupt enable (enabled)
+	val = (0 << 7 /*FEIE*/);	// FIFO error interrupt enable (off)
 	val |= (1 << 2 /*DMDIS*/);	// Direct mode disable (disabled)
 	val |= (3 << 0 /*FTH*/);	// FIFO threshold selection (full)
 	reg_rmw(&dma->sregs->FCR, DMA_SxFCR_MASK, val);
+
+	// clear interrupt flags
+	dma_clr_interrupt_flags(dma);
 
 	return 0;
 }
--- soc/st/stm32f4/lib/dma.h
+++ soc/st/stm32f4/lib/dma.h
@@ -17,6 +17,14 @@ DMA Driver
 
 //-----------------------------------------------------------------------------
 
+typedef struct {
+	volatile uint32_t ISR;	// DMA interrupt status register
+	volatile uint32_t reserved;
+	volatile uint32_t IFCR;	// DMA interrupt flag clear register
+} DMA_Interrupt_TypeDef;
+
+//-----------------------------------------------------------------------------
+
 struct dma_cfg {
 	int controller;		// dma controller to use
 	int stream;		// dma stream to use
@@ -26,13 +34,26 @@ struct dma_cfg {
 
 struct dma_drv {
 	struct dma_cfg cfg;
-	DMA_TypeDef *cregs;	// controller registers
+	DMA_Interrupt_TypeDef *iregs;	// interrupt registers
 	DMA_Stream_TypeDef *sregs;	// stream registers
 };
 
 //-----------------------------------------------------------------------------
 
+// enable a dma stream
+static inline void dma_enable(struct dma_drv *dma) {
+	dma->sregs->CR |= (1 << 0 /*EN*/);
+}
+
+// disable a dma stream
+static inline void dma_disable(struct dma_drv *dma) {
+	dma->sregs->CR &= ~(1 << 0 /*EN*/);
+}
+
+//-----------------------------------------------------------------------------
+
 int dma_init(struct dma_drv *dma, struct dma_cfg *cfg);
+int dma_start(struct dma_drv *dma, uint32_t dst, uint32_t src, uint32_t length);
 
 //-----------------------------------------------------------------------------
 
